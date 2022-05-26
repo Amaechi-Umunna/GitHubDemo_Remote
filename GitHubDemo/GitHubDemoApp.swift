5bb4c53fc70884e4b7fc8efb227cc0be02136fd8
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-05-26 06:15:08 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Amaechi Chukwu on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
